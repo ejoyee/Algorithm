7dce282f6d40ba5865d7e0c57c1346a3cb3cc2fd
ejoyee <105860526+ejoyee@users.noreply.github.com>
2024-08-28 16:24:03 +0900

    [Gold V] Title: 집합의 표현, Time: 308 ms, Memory: 48652 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	static int n;
+	static int m;
+	
+	static int[] parents;
+	
+	static void make() {
+		
+		for (int i = 1; i <= n; i++) {
+			parents[i] = i;
+		}
+		
+	}
+	
+	static int findSet(int a) {
+		
+		if (a == parents[a]) return a;
+		
+		return parents[a] = findSet(parents[a]);
+		
+	}
+	
+	static boolean union(int a, int b) {
+		
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		
+		if (aRoot == bRoot) return false;	// 두 집합의 대표자가 같으면 이미 같은 집합이므로, 합집합 연산 불가
+
+		// aRoot에 bRoot를 흡수
+		// 대표자 - 대표자끼리 합치기
+		parents[bRoot] = aRoot;
+		return true;
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			
+			parents = new int[n+1];
+			make();
+			
+			for (int i = 0; i < m; i++) {
+				
+				st = new StringTokenizer(br.readLine());
+				
+				int cmd = Integer.parseInt(st.nextToken());
+				
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				if (cmd == 0) {
+					
+					union(a, b);
+					
+				}
+				else if (cmd == 1) {
+					
+					if (findSet(a) == findSet(b)) {
+						sb.append("YES").append('\n');
+					}else {
+						sb.append("NO").append('\n');
+					}
+					
+				}
+				
+			}
+			
+		
+		System.out.println(sb);
+	}	// main end
+}	// class end
