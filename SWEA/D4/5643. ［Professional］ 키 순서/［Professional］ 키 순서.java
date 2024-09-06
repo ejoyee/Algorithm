f574d62067d3e7843e9d7428e9f93eda8bc9bdf1
ejoyee <105860526+ejoyee@users.noreply.github.com>
2024-09-06 10:08:06 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,970 ms, Memory: 91,584 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -6,100 +6,74 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
+
 	static StringBuilder sb = new StringBuilder();
-	
-	static int N, adjMartix[][];
-	
+
+	static int N, adjMartix[][], cnt;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine().trim());
-		
+		StringTokenizer st; // 졸려
+
+		int T = Integer.parseInt(br.readLine().trim()); // 매우 졸려
+
 		for (int t = 1; t <= T; t++) {
-			
-			N = Integer.parseInt(br.readLine());
+
+			N = Integer.parseInt(br.readLine()); // 너무 졸려
 			int M = Integer.parseInt(br.readLine());
-			
-			adjMartix = new int[N+1][N+1]; // 학생 번호가 1번부터 시작
+
+			adjMartix = new int[N + 1][N + 1]; // 학생 번호가 1번부터 시작
 			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
+				st = new StringTokenizer(br.readLine()); // 진짜 졸려
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				adjMartix[a][b] = 1;
 			}
-			
-			int ans = 0;	// 자신의 키를 알 수 있는 학생 수 
-			
+
+			int ans = 0; // 자신의 키를 알 수 있는 학생 수
+
 			// 각 학생마다 자신보다 큰, 자신보다 작은 학생 각각 탐색
 			for (int i = 1; i <= N; i++) {
-				if (gtBFS(i) + ltBFS(i) == N-1)
-					ans++;
+				cnt = 0;
+				gtDFS(i, new boolean[N+1]);
+				ltDFS(i, new boolean[N+1]);
+				if (cnt == N-1) ans++;
 			}
-			
+
 			sb.append('#').append(t).append(' ').append(ans).append('\n');
 		}
 		System.out.println(sb);
 	} // main end
-	
+
 	// 자신보다 큰 쪽으로 따라 탐색
-	private static int gtBFS(int start) {
-		
-		int cnt = 0; // 나보다 큰 애들만 카운팅
-		Queue<Integer> q = new ArrayDeque<>();
-		boolean[] visited = new boolean[N+1];
-		
-		q.offer(start);
-		visited[start] = true;
-		
-		while(!q.isEmpty()) {
-			
-			int cur = q.poll();
-			for (int i = 1; i <= N; i++) {
-				
-				// 나보다 큰 애
-				if (!visited[i] && adjMartix[cur][i] != 0) {
-					q.offer(i);
-					visited[i] = true;
-					cnt++; // 나보다 큰 대상 카운팅
-				}
+	private static void gtDFS(int cur, boolean[] visited) {
+
+		visited[cur] = true;
+		for (int i = 1; i <= N; i++) {
+			// 나보다 큰 애
+			if (!visited[i] && adjMartix[cur][i] != 0) {
+				gtDFS(i, visited);
+				cnt++; // 나보다 큰 대상 카운팅
 			}
 		}
-		
-		return cnt;
-	}	// gtBFS end
-	
-	
+ 
+	} // gtDFS end
+
 	// 자신보다 작은 쪽으로 따라 탐색
-	// 열로 접근 
-	private static int ltBFS(int start) {
+	// 열로 접근
+	private static void ltDFS(int cur, boolean[] visited) {
+
+		visited[cur] = true;
 		
-		int cnt = 0; // 나보다 큰 애들만 카운팅
-		Queue<Integer> q = new ArrayDeque<>();
-		boolean[] visited = new boolean[N+1];
-		
-		q.offer(start);
-		visited[start] = true;
-		
-		while(!q.isEmpty()) {
-			
-			int cur = q.poll();
-			for (int i = 1; i <= N; i++) {
-				
-				// 나보다 작은 애 [i][cur]만 바꾸면 됨요
-				if (!visited[i] && adjMartix[i][cur] != 0) {
-					q.offer(i);
-					visited[i] = true;
-					cnt++; // 나보다 큰 대상 카운팅
-				}
+		for (int i = 1; i <= N; i++) {
+			// 나보다 큰 애
+			if (!visited[i] && adjMartix[i][cur] != 0) {
+				ltDFS(i, visited);
+				cnt++; // 나보다 큰 대상 카운팅
 			}
 		}
-		
-		return cnt;
-	}	// gtBFS end
-	
-	
-	
-}	// class end
+
+	} // ltDFS end
+
+} // class end
