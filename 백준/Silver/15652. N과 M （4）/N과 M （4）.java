1db871b6a1aa137a3ea11b97794a56ac723b9555
ejoyee <105860526+ejoyee@users.noreply.github.com>
2024-09-30 15:12:19 +0900

    [Silver III] Title: N과 M (4), Time: 76 ms, Memory: 12416 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	static int N, M;
+	static int[] arr;
+	
+	static void dfs(int cnt, int start) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for (int i = start; i <= N; i++) {
+			arr[cnt] = i;
+			dfs(cnt+1, i);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		
+		dfs(0, 1);
+		System.out.println(sb);
+	}
+}
