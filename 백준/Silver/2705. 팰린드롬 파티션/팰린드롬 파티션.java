ded60d6548827c414026864a06af8dd42a5f2602
ejoyee <105860526+ejoyee@users.noreply.github.com>
2024-10-01 22:20:37 +0900

    [Silver I] Title: 팰린드롬 파티션, Time: 60 ms, Memory: 11332 KB -BaekjoonHub

--- 백준/Silver/2705. 팰린드롬 파티션/팰린드롬 파티션.java
+++ 백준/Silver/2705. 팰린드롬 파티션/팰린드롬 파티션.java
@@ -4,33 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static StringBuilder sb = new StringBuilder();
-	
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int comp = 3;
-		int[] dp = new int[1001];
-		dp[1] = 1;
-		dp[2] = 2;
-		
 		int T = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= T; t++) {
+		int[] nums = new int[T];
+		int maxnum = 0;
+		for (int t = 0; t < T; t++) {
+			nums[t] = Integer.parseInt(br.readLine());
+			if (maxnum < nums[t]) maxnum = nums[t];
+		}
+		
+		int[] dp = new int[maxnum+1];
+		
+		dp[1] = 1;
+		if (maxnum > 1) dp[2] = 2;
+		
+		int comp = 3;
+		
+		for (int t = 0; t < T; t++) {
 			
-			int N = Integer.parseInt(br.readLine());
-			
-			for (int i = comp; i <= N; i++) {
+			for (int i = comp; i <= nums[t]; i++) {
 				if (i % 2 == 0) {
-					dp[i] = dp[i-1] + dp[i/2];
-				}
-				else {
-					dp[i] = dp[i-1];
+					dp[i] = dp[i - 1] + dp[i / 2];
+				} else {
+					dp[i] = dp[i - 1];
 				}
 			}
-			comp = Math.max(comp, N);
-			sb.append(dp[N]).append('\n');
+			comp = Math.max(comp, nums[t]);
+			sb.append(dp[nums[t]]).append('\n');
 		}
 		System.out.println(sb);
 	}
