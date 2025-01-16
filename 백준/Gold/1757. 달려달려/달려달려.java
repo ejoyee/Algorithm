941e7c8da7365c7b33e29f85b3eb3fa9aba18d3c
ejoyee <105860526+ejoyee@users.noreply.github.com>
2025-01-16 22:33:05 +0900

    [Gold IV] Title: 달려달려, Time: 356 ms, Memory: 150508 KB -BaekjoonHub

--- 백준/Gold/1757. 달려달려/달려달려.java
+++ 백준/Gold/1757. 달려달려/달려달려.java
@@ -17,12 +17,13 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[][][] dp = new int[N+1][M+1][2];	// N번의 시간 - N분째 달렸을 때 지침지수, 달리기(0)/쉬기(1) => 지금까지 달린 거리
+		int[][][] dp = new int[N+1][M+1][2];	// N번의 시간 - N분째 달렸을 때 지침지수 - 달리기(0)/쉬기(1) => 지금까지 달린 거리
 		
 		int result = 0;
 		for (int i = 1; i <= N; i++) {
 			int num = Integer.parseInt(br.readLine());
 			
+			// 지침지수를 늘려가며
 			for (int j = 0; j < i; j++) {
 				// 쉬기 (달리던 중일수도 쉬던 중일수도)
 				dp[i][j > 0 ? j-1 : j][1] = Math.max(dp[i-1][j][1], dp[i][j > 0 ? j-1 : j][1]);	// 마저 쉴 때
@@ -30,9 +31,12 @@ public class Main {
 				
 				// 달리기
 				if (j == M) break;	// 지침지수 넘으면 안 돼서 
-				dp[i][j < N ? j+1 : j][0] = dp[i-1][j][0] + num;	// 달리는 상태일 때
-				// 지침지수가 0이라면 쉬던 상태여도 이젠 달려도 됨
-				if(j == 0) dp[i][j < N ? j+1 : j][0] = Math.max(dp[i-1][j][1] + num, dp[i][j < N ? j+1 : j][0]);
+				if (j < N) {
+					dp[i][j+1][0] = dp[i-1][j][0] + num;	// 달리는 상태일 때
+					// 지침지수가 0이라면 쉬던 상태여도 이젠 달려도 됨
+					if(j == 0) dp[i][j+1][0] = Math.max(dp[i-1][j][1] + num, dp[i][j+1][0]);
+				}
+				
 			}
 			
 			
