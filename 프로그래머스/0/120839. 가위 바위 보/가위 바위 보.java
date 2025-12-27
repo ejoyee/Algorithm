2abb1f9e7b631d044f570a81968d9450384cba4c
ejoyee <105860526+ejoyee@users.noreply.github.com>
2025-12-28 01:32:12 +0900

    [level 0] Title: 가위 바위 보, Time: 2.22 ms, Memory: 97.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        Map<Character, Character> map = new HashMap<>();
+        map.put('2', '0');
+        map.put('0', '5');
+        map.put('5', '2');
+        
+        for (int i = 0; i < rsp.length(); i++){
+            answer += map.get(rsp.charAt(i));
+        }
+        return answer;
+    }
+}
