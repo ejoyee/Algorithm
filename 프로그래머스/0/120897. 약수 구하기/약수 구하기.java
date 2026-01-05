e41a1195f73982906ee967be059c83ade9cba4f3
ejoyee <105860526+ejoyee@users.noreply.github.com>
2026-01-05 19:48:53 +0900

    [level 0] Title: 약수 구하기, Time: 2.96 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        
+        Collections.sort(list);
+        answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
