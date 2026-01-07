212b39a57834b42c004d9c419d53eb11fd8f410d
ejoyee <105860526+ejoyee@users.noreply.github.com>
2026-01-07 22:42:12 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        String str = Integer.toString(order);
+        
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if (c == '3' || c == '6' || c == '9') {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
