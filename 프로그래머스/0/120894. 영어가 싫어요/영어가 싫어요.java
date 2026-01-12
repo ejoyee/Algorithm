1ae8339d2f459c41f294cbd2ba77a49149547830
ejoyee <105860526+ejoyee@users.noreply.github.com>
2026-01-12 21:52:34 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(String numbers) {
+        
+        String[] nums = {
+            "zero","one","two","three","four",
+            "five","six","seven","eight","nine"
+        };
+
+        for (int i = 0; i < nums.length; i++) {
+            numbers = numbers.replace(nums[i], String.valueOf(i));
+        }
+
+        // Integer로 하면 터짐
+        return Long.parseLong(numbers);
+    }
+}
