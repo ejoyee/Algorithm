3e5568ecec274e584e671939e349ce3c1730ad76
ejoyee <105860526+ejoyee@users.noreply.github.com>
2026-05-26 14:21:52 +0900

    [level 0] Title: 모음 제거, Time: 7.55 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (int i = 0; i < my_string.length(); i++){
+            char tmp = my_string.charAt(i);
+            
+            if (tmp == 'a' || tmp == 'e' || tmp == 'i' || tmp == 'o' || tmp == 'u') {
+                continue;
+            }
+            else {
+                answer += tmp;
+            }
+        }
+        
+        return answer;
+    }
+}
